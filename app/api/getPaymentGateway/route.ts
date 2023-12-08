db8b56f0e373dc9e7c865a07b5eec8f2109d6467
TKPclothing <135633071+TKPclothing@users.noreply.github.com>
2023-12-08 14:56:21 +0530

    Update route.ts

--- app/api/getPaymentGateway/route.ts
+++ app/api/getPaymentGateway/route.ts
@@ -2,6 +2,38 @@ import { getProduct } from "@/sanity/sanity-utils";
 import { SHA256, enc } from "crypto-js";
 import axios from "axios";
 
+function uuid() {
+  const srr = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "X",
+    "Y",
+    "Z",
+  ];
+  return [...new Array(35)]
+    .map(() => srr[Math.floor(Math.random() * srr.length)])
+    .toString();
+}
 export async function POST(request: Request) {
   try {
     //const price = (await getProduct(request.url.split("/").at(-1) || "")).price;
@@ -11,7 +43,7 @@ export async function POST(request: Request) {
     const payload = {
       merchantId: process.env.NEXT_PRIVATE_MERCHANTID,
       merchantTransactionId: orderId,
-      merchantUserId: process.env.NEXT_PRIVATE_MERCHANTUSERID,
+      merchantUserId: uuid(),
       //amount: parseFloat(`${price}.00`) * 100,
       amount: parseFloat(`${price}`) * 100,
       redirectUrl: `${process.env.NEXT_PHONEPE_REDIRECT}/api/paymentConfrom/${order_id}`,
@@ -30,7 +62,7 @@ export async function POST(request: Request) {
     let salt_key = process.env.NEXT_PRIVATE_PHONEPE_SALT_KEY;
     let salt_index = process.env.NEXT_PRIVATE_SALT_INDEX;
     let res = await axios.post(
-      "https://api.phonepe.com/apis/hermes/pg/v1/pay",
+      "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay",
       {
         request: data.toString("base64"),
       },
