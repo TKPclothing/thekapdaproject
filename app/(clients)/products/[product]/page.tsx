d0eb117ae2fb4ef4bfb9f37d74d14643a2d0fb16
Sahil Satpute <explore.sahil1@gmail.com>
2024-01-08 17:49:26 +0530

    Update page.tsx

--- app/(clients)/products/[product]/page.tsx
+++ app/(clients)/products/[product]/page.tsx
@@ -47,7 +47,7 @@ export default function Product({ params }: Props) {
 
     fetchRelatedProducts();
 
-    console.log(relatedProducts);
+    //console.log(relatedProducts);
   }, []);
 
   useEffect(() => {
